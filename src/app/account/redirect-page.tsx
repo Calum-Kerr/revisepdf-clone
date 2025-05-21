296e2132ccf8728df8cf5065a40e2c572ebf13c3
Calum Kerr <calum.kerr@example.com>
2025-05-21 21:56:58 +0100

    test

--- src/app/account/redirect-page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-import { defaultLocale } from '@/i18n';
-
-export const dynamic = 'force-dynamic';
-
-export default function AccountRedirectPage() {
-  redirect(`/${defaultLocale}/account`);
-}
